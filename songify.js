635578f49c205ba74b517bb760e2cfa3d87e29e1
Nitin Gupta <gupta7nitin@gmail.com>
2019-08-16 22:57:34 +0530

    First Commit

---
+++ songify.js
@@ -0,0 +1,10 @@
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+prev.addEventListener('click',function(event){
+	alert("Previous Clicked");
+});
+
+next.addEventListener('click',function(event){
+	alert("Next Clicked");
+});
